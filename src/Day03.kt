9908f4fbb2f684c40066dd8a20df74d5809640f4
CUART Bruno <bcuart@jouve.com>
2021-12-07 12:50:46 +0100

    Day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -4,18 +4,18 @@ class Day03 {
     fun part1(input: List<String>): Int {
 
         // pair of counters : first for '0' and second for '1'
-        val values = ArrayList<Pair<Int,Int>>()
+        val values = ArrayList<Pair<Int , Int>>()
 
         repeat(input[0].length) {
-            values.add(Pair(0, 0))
+            values.add(Pair(0 , 0))
         }
 
         input.forEach {
             for (i in it.indices) {
                 val pair = when (it[i]) {
-                    '0' -> values[i].copy(first = values[i].first+1)
-                    '1' -> values[i].copy(second = values[i].second+1)
-                    else -> Pair(0,0)
+                    '0' -> values[i].copy(first = values[i].first + 1)
+                    '1' -> values[i].copy(second = values[i].second + 1)
+                    else -> Pair(0 , 0)
                 }
                 values[i] = pair
             }
@@ -34,15 +34,74 @@ class Day03 {
             }
         }
 
-        val gamma = Integer.parseInt(binaryGamma.toString(),2)
-        val epsilon = Integer.parseInt(binaryEpsilon.toString(),2)
+        val gamma = Integer.parseInt(binaryGamma.toString() , 2)
+        val epsilon = Integer.parseInt(binaryEpsilon.toString() , 2)
 
-        return gamma*epsilon
+        return gamma * epsilon
     }
 
     // https://adventofcode.com/2021/day/3#part2
     fun part2(input: List<String>): Int {
 
-        return input.size
+        var binaryOxygen = input
+        var binaryCO2 = input
+
+        var charIndex= 0
+        while (binaryOxygen.size > 1) {
+            binaryOxygen = binaryOxygen.filter {
+                it[charIndex] == mostCommonCharacter(binaryOxygen, charIndex)
+            }
+            charIndex++
+        }
+
+        charIndex = 0
+        while (binaryCO2.size > 1) {
+            binaryCO2 = binaryCO2.filter {
+                it[charIndex] == leastCommonCharacter(binaryCO2, charIndex)
+            }
+            charIndex++
+        }
+
+        // get resulted binaries
+        val oxygen = Integer.parseInt(binaryOxygen[0], 2)
+        val co2 = Integer.parseInt(binaryCO2[0], 2)
+
+        return oxygen*co2
+    }
+
+    private fun mostCommonCharacter(array: List<String> , bitIndex: Int): Char {
+        val counter = compute0and1(array , bitIndex)
+        return if (counter.second >= counter.first) {
+            '1'
+        } else {
+            '0'
+        }
+    }
+
+    private fun leastCommonCharacter(array: List<String> , bitIndex: Int): Char {
+        val counter = compute0and1(array , bitIndex)
+        return if (counter.first <= counter.second) {
+            '0'
+        } else {
+            '1'
+        }
+    }
+
+    private fun compute0and1(array: List<String> , bitIndex: Int): Pair<Int , Int> {
+
+        var counter0 = 0
+        var counter1 = 0
+
+        for (binary in array) {
+            if (binary.isNotEmpty()) {
+
+                when (binary[bitIndex]) {
+                    '0' -> counter0 += 1
+                    else -> counter1 += 1
+                }
+            }
+        }
+
+        return Pair(counter0 , counter1)
     }
 }
