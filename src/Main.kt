959fea6fdd3c76a32788b1e4e639fa94489789bc
CUART Bruno <bcuart@jouve.com>
2021-12-07 11:39:51 +0100

    refactor launch of Day01

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,12 @@
 fun main() {
-    var day = Day01()
+
 
     //val testInput = readInput("Day01_test")
     //check(day.part1(testInput) == 1)
 
+    val day01 = Day01()
     val input = readInput("Day01")
-    println("Result of part 1: ${day.part1(input)}")
-    println("Result of part 2: ${day.part2(input)}")
+    println("Result of part 1: ${day01.part1(input)}")
+    println("Result of part 2: ${day01.part2(input)}")
 
 }
