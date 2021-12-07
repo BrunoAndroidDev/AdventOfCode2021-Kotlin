2f77068032892e7dbefd31906c0293c7bee81fa0
CUART Bruno <bcuart@jouve.com>
2021-12-07 11:28:39 +0100

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
-fun main() {
+class Day01 {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        // Variables initialization
+        var counter = 0
+
+        if (input.isNotEmpty()) {
+            var value = Integer.valueOf(input[0])
+            var previous = value
+            for (item in input) {
+                value = Integer.valueOf(item)
+                if (value > previous) {
+                    counter++
+                }
+                previous = value
+            }
+        }
+        return counter
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
 }
---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var day = Day01()
+
+    //val testInput = readInput("Day01_test")
+    //check(day.part1(testInput) == 1)
+
+    val input = readInput("Day01")
+    println(day.part1(input))
+    //println(day.part2(input))
+}
