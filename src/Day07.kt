19d78d07457cd5f3b3e6468c9777625c9d0e7ee5
CUART Bruno <bcuart@jouve.com>
2021-12-09 18:16:16 +0100

    day 7 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,10 +1,18 @@
 import kotlin.math.abs
+import kotlin.math.min
+import kotlin.math.roundToInt
 
 fun List<Int>.median(): Int =
     if (this.size % 2 == 0)
         (this[this.size / 2] + this[(this.size - 1) / 2]) / 2
     else
         this[this.size / 2]
+
+fun List<Int>.sumToAverage(average: Int) =
+    this.sumOf {
+        val diff = abs(it - average)
+        diff * (1 + diff) / 2
+    }
 
 class Day07 {
 
@@ -24,7 +32,24 @@ class Day07 {
     // https://adventofcode.com/2021/day/7#part2
     fun part2(input: List<String>): Int {
 
-        return input.size
+        // convert input
+        val init = input[0].split(",").map { it.toInt() }
+
+        // get average
+        val averageFloor = init.average().toInt()
+        val averageCeiling = init.average().roundToInt()
+
+        // compute fuel
+        val fuelToFloor = init.sumToAverage(averageFloor)
+        val fuelToCeiling = init.sumToAverage(averageCeiling)
+        val fuelToAverage = if (averageFloor != averageCeiling) {
+            min(fuelToFloor, fuelToCeiling)
+        } else {
+            fuelToFloor
+        }
+
+        // return the fuel consumption
+        return fuelToAverage
     }
 
 }
