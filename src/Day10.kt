96c828eaad8d94813717a6e1dbbc0ac1c9a12419
CUART Bruno <bcuart@jouve.com>
2021-12-14 18:26:25 +0100

    Day 10 part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,15 +1,15 @@
 class Day10 {
 
-    private val openChars = "([{<"
-    private val closeChars = mutableMapOf(
-        ')' to Pair('(',3),
-        ']' to Pair('[',57),
-        '}' to Pair('{',1197),
-        '>' to Pair('<',25137)
-    )
-
     // https://adventofcode.com/2021/day/10#part1
     fun part1(input: List<String>): Int {
+
+        val openChars = "([{<"
+        val closeChars = mutableMapOf(
+            ')' to Pair('(',3),
+            ']' to Pair('[',57),
+            '}' to Pair('{',1197),
+            '>' to Pair('<',25137)
+        )
 
         var score = 0
 
@@ -33,8 +33,47 @@ class Day10 {
     }
 
     // https://adventofcode.com/2021/day/10#part2
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
 
-        return input.size
+        val openChars = mutableMapOf(
+            '(' to Pair(')',1),
+            '[' to Pair(']',2),
+            '{' to Pair('}',3),
+            '<' to Pair('>',4)
+        )
+        val closeChars = mutableMapOf(
+            ')' to '(',
+            ']' to '[',
+            '}' to '{',
+            '>' to '<'
+        )
+
+        val scoreTabs = mutableSetOf<Long>()
+
+        input.forEach { line ->
+            var skip = false
+            val chunks = StringBuilder()
+            for (char in line) {
+                if (openChars.keys.contains(char)) {
+                    chunks.append(char)
+                } else {
+                    if (chunks.last() == closeChars[char]!!) {
+                        chunks.deleteCharAt(chunks.length-1)
+                    } else {
+                        skip = true
+                        break
+                    }
+                }
+            }
+            if (!skip) {
+                var score = 0L
+                chunks.reverse().forEach {
+                    score = score * 5 + openChars[it]!!.second
+                }
+                scoreTabs.add(score)
+            }
+        }
+
+        return scoreTabs.sorted()[scoreTabs.size/2]
     }
 }
